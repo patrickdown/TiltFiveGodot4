ed4186e873671951138bef147ef2bdab6cf9b871
patrickdown <patrick.down@gmail.com>
2024-05-19 10:19:17 -0400

    Add Tangible Tracking Camera support
    Added enabling the tangible camera and reading frames and pose
    Added the T5ImageCapture node to interface with the camera
    Update example.gd and example.csharp to demo new features

--- example.gd/main.gd
+++ example.gd/main.gd
@@ -1,9 +1,47 @@
 extends Node3D
 
+var image_capture: T5ImageCapture
+var is_capturing := false
+var image_size: Vector2i
+var camera_image: Image
+@onready var camera_view: TextureRect = $ScreenUI/CameraView
 
-func _on_t5_manager_xr_rig_was_added(xr_rig):
-	print("Scene for glasses ", xr_rig.get_glasses_id(), " added")
+func _on_t_5_manager_xr_rig_was_added(xr_rig: T5XRRig):
+	image_capture = xr_rig.get_image_capture()
 
+func _on_t_5_manager_xr_rig_will_be_removed(_xr_rig):
+	image_capture.stop_capture()
+	is_capturing = false
+	camera_view.visible = false
+	image_capture = null
 
-func _on_t5_manager_xr_rig_will_be_removed(xr_rig):
-	print("Scene for glasses ", xr_rig.get_glasses_id(), " removed")
+func _input(event):
+	if image_capture == null:
+		return
+	if not event.is_action_pressed("toggle_camera"):
+		return
+	if not is_capturing and image_capture.start_capture():
+		print("start capture")
+		is_capturing = true
+		camera_view.visible = true
+	else:
+		print("stop capture")
+		image_capture.stop_capture()
+		is_capturing = false
+		camera_view.visible = false
+
+func _process(_delta):
+	if not is_capturing:
+		return
+	if image_capture.acquire_buffer():
+		var buffer := image_capture.get_image_data()
+		var new_size = image_capture.get_image_size()
+		var new_illumination_mode = image_capture.get_frame_illumination_mode()
+		if camera_image == null or image_size != new_size:
+			image_size = new_size
+			camera_image = Image.create_from_data(image_size.x, image_size.y, false, Image.FORMAT_R8, buffer)
+			camera_view.texture = ImageTexture.create_from_image(camera_image)
+		else:
+			camera_image.set_data(image_size.x, image_size.y, false, Image.FORMAT_R8, buffer)
+			camera_view.texture.update(camera_image)
+		image_capture.release_buffer()
--- extension/src/TiltFiveXRInterface.cpp
+++ extension/src/TiltFiveXRInterface.cpp
@@ -1,4 +1,5 @@
 #include "TiltFiveXRInterface.h"
+#include <T5ImageCapture.h>
 #include <godot_cpp/classes/engine.hpp>
 #include <godot_cpp/classes/os.hpp>
 #include <godot_cpp/classes/project_settings.hpp>
@@ -251,8 +252,19 @@ void TiltFiveXRInterface::_start_display(TiltFiveXRInterface::GlassesIndexEntry&
 	entry.viewport_id = viewport->get_instance_id();
 	entry.gameboard_id = gameboard->get_instance_id();
 
+	_setup_scene_nodes(glasses, gameboard);
+
 	viewport->set_use_xr(true);
 	viewport->set_update_mode(godot::SubViewport::UpdateMode::UPDATE_ALWAYS);
+}
+
+void TiltFiveXRInterface::_setup_scene_nodes(GodotT5Glasses::Ptr glasses, Node* node) {
+	for (int i = 0; i < node->get_child_count(); i++) {
+		auto image_capture = Object::cast_to<T5ImageCapture>(node->get_child(i));
+		if (image_capture) {
+			image_capture->set_glasses(glasses);
+		}
+	}
 }
 
 void TiltFiveXRInterface::stop_display(const StringName glasses_id) {
@@ -310,7 +322,7 @@ PackedStringArray TiltFiveXRInterface::get_reserved_glasses_ids() {
 }
 
 String TiltFiveXRInterface::get_glasses_name(const StringName glasses_id) {
-	if(!t5_service)
+	if (!t5_service)
 		return String("");
 
 	auto entry = lookup_glasses_entry(glasses_id);
